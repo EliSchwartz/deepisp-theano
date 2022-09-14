fb074bb09ad5b0d8a8f93e48b893785285fac698
EliSchwartz <eli.shw@gmail.com>
2022-09-14 22:24:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,11 @@ Created on Thu Feb 04 12:23:14 2016
 
 
 import os
-# os.environ["CPATH"] = '/usr/local/cudnn/4.0.7:'#+os.environ["CPATH"]
-# os.environ["LIBRARY_PATH"] = '/usr/local/cudnn/4.0.7:'#+os.environ["LIBRARY_PATH"]
-# os.environ["LD_LIBRARY_PATH"] = '/usr/local/cuda-7.5/lib64:/usr/local/cudnn/4.0.7:'#+os.environ["LD_LIBRARY_PATH"]
-# os.environ["THEANO_FLAGS"] = 'mode=FAST_RUN,device=gpu2,floatX=float32,dnn.enabled=True'
 
 OVERRIDE_OUTPUT_DIR = False
 
 exp_name = 'base'
+output_dir = '/data/eli/experiments/output/' + exp_name
 
 train_params = {
     'MAX_EPOCHS'                    :   0             ,
@@ -60,7 +57,6 @@ import time
 
 
 cur_file_name = os.path.basename(__file__)[:-3]
-output_dir = '/data/eli/experiments/output/' + exp_name
 
 if os.path.exists(output_dir) and OVERRIDE_OUTPUT_DIR:
     shutil.rmtree(output_dir)
@@ -77,30 +73,12 @@ if os.path.exists(training_stats_path):
                 for row in csv.DictReader(f, skipinitialspace=True)]
 else:
     training_stats = []
-
-    
-#ds = raw2rgb_dataset.Raw2RGB_fullimage_Dataset(
-#            dir_path = '/data/eli/datasets/fi_dataset/orig/',
-#            in_image = 'short_exposure1.png',
-#            target_image = 'medium_exposure.jpg',)
-#ds = raw2rgb_dataset.CorrectRGB_uniform_w_noise_Dataset(std=0, quadratic_color_trans = True)
-#        in_image='short_exposure1.png', target_image = 'short_exposure.jpg')
-
-#Xt = Xt[:,:,:,504:-504]
-#yt = yt[:,:,:,504:-504]
-
-#Xt = Xt[:,:,:2048,:2048]
-#yt = yt[:,:,:2048,:2048]
-#X = np.random.standard_normal([10,3,256,256]).astype(np.float32)
-#y = np.random.standard_normal([10,3,256,256]).astype(np.float32)
     
 def test_net(nn, train_history=[]):
     
     epoch = len(train_history)
     if params['TEST_EVERY_N_EPOCHS'] and not epoch%params['TEST_EVERY_N_EPOCHS'] == 0:
-        return     
-
-#    testing_net.load_params_from(params_path)
+        return
     
     print('Starting testing on selected test images \n') 
     out_dir = os.path.join(output_dir, str(epoch))
@@ -133,10 +111,6 @@ def test_net(nn, train_history=[]):
     X_ = X_.clip(0, 1)
     y_ = y_.clip(0, 1)
     y_h = y_h.clip(0, 1)
-    
-#    X_ = utils.srgb_gamma(X_)
-#    y_ = utils.srgb_gamma(y_)
-#    y_h = utils.srgb_gamma(y_h)
 
     X_ = (255*X_).astype(np.uint8)
     y_ = (255*y_).astype(np.uint8)
@@ -154,13 +128,10 @@ def test_net(nn, train_history=[]):
 #            y_[i,...])
     
     nn.save_params_to(out_dir + '/params.pickle')        
-#    pickle.dump( nn, open(out_dir + '/net.pickle', "wb" ) )
     
         
         
 on_epoch_finished = [
-#        nl.handlers.SaveWeights(path = params_path, every_n_epochs = 10, pickle = False),
-#        nl.handlers.RememberBestWeights()
         utils.SaveNetState(
             training_stats_path, 
             params_path,
@@ -180,9 +151,6 @@ class LoadPatchesBatchIterator(nl.BatchIterator):
         Xb, yb = shuffle(Xb,yb)
         Xb[:half,...] = Xb[:half,:,:,::-1]  
         yb[:half,...] = yb[:half,:,:,::-1]  
-#        Xb, yb = shuffle(Xb,yb)
-#        Xb[:half,...] = Xb[:half,:,::-1,:]  
-#        yb[:half,...] = yb[:half,:,::-1,:]  
         
         out_x = np.zeros(Xb.shape[:2] + (params['IN_PATCH_SIZE'], params['IN_PATCH_SIZE']), dtype=np.float32)
         out_y = np.zeros_like(out_x)
@@ -198,8 +166,6 @@ num_epochs_to_do = max(params["TRAIN_PARAMS"]["MAX_EPOCHS"] - len(training_stats
 print('Loading dataset...')   
 t = time.time()
 hf = h5py.File('/data/eli/datasets/full_images.h5', 'r')
-# X = hf['X'][:]
-# y = hf['y'][:]
 Xt = hf['Xt'][:]
 yt = hf['yt'][:]
 
@@ -218,15 +184,11 @@ layers = models.denoising_and_coloring( (None,3,params['IN_PATCH_SIZE'],params['
                                         num_coloring_layers=3,
                                         color_trans_init=color_trans_init)
 
-#from common import models
 net = nl.NeuralNet(
     layers                  =   layers,#models.color_trans_estim((None,3,256,256)),
 
     update                  =   lsgn.updates.adam, #nesterov_momentum,
     update_learning_rate    =   theano.shared(utils.float32(params["TRAIN_PARAMS"]["LEARNING_RATE_INITIAL"])),
-    #update_momentum         =   theano.shared(utils.float32(params["TRAIN_PARAMS"]["MOMENTUM_INITIAL"])),
-
-#        objective = custom_losses.objective_with_vgg(vgg_alpha=1.0),
     objective_loss_function =   custom_losses.loss_Lab(vgg_alpha=0.5),
     
     regression              =   True,
@@ -235,7 +197,6 @@ net = nl.NeuralNet(
     max_epochs              =   num_epochs_to_do,
     train_split             =   nl.TrainSplit(eval_size=params["TRAIN_PARAMS"]["TRAIN_VALID_SPLIT"]),
     on_epoch_finished       =   on_epoch_finished,
-#    on_training_started     =   on_training_started,
     y_tensor_type           =   T.TensorType(theano.config.floatX, (False,)*4),
     check_input             =   False,
     verbose                 =   1,
@@ -251,31 +212,9 @@ net.train_history_ = training_stats
 print('Starting training {} epochs\n'.format(num_epochs_to_do))
 if num_epochs_to_do:
     net.fit(X, y)
-#else:
-#print('Runing test set', flush = True)
-#    Xt, yt, fnames = ds.get_test_samples()  
-#    Xt, yt = ds.get_training_samples(3)
-#    fnames = ['train0', 'train1', 'train2']
 
     
 #### Testing net ##################
-
-#layers = models.denoising_and_coloring((None,3,Xt.shape[-2],Xt.shape[-1]), 
-#                                       flatten=False, num_denoise_layers=0,
-#                                       num_coloring_layers=5)
-#
-#net = nl.NeuralNet(
-#    layers                  =   layers,#models.color_trans_estim((None,3,256,256)),
-#    update                  =   lsgn.updates.adam, #nesterov_momentum,
-#    update_learning_rate    =   theano.shared(utils.float32(params["TRAIN_PARAMS"]["LEARNING_RATE_INITIAL"])),
-#    regression              =   True,
-#    check_input             =   False,
-#    y_tensor_type           =   T.TensorType(theano.config.floatX, (False,)*4),
-#    batch_iterator_test     =   nl.BatchIterator(batch_size = 1),
-#    )
-#net.initialize()
-#net.load_params_from(params_path)
-
 test_net(net)   
    
 
